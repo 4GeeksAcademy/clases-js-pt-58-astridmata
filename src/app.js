58d7fe79091669fa97a87097eca1ac7e37d18810
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2025-09-25 02:36:49 +0000

    objetos, operadores ternarios

--- src/app.js
+++ src/app.js
@@ -177,134 +177,219 @@ window.onload = function () {
 
 
   // }
-//Semana 1: Lun Mar Mie Jue Vie Sab Dom  
-//Semana 2: Lun Mar Mie Jue Vie Sab Dom  
-//Semana 3: Lun Mar Mie Jue Vie Sab Dom  
-//Semana 4: Lun Mar Mie Jue Vie Sab Dom  
+  //Semana 1: Lun Mar Mie Jue Vie Sab Dom  
+  //Semana 2: Lun Mar Mie Jue Vie Sab Dom  
+  //Semana 3: Lun Mar Mie Jue Vie Sab Dom  
+  //Semana 4: Lun Mar Mie Jue Vie Sab Dom  
 
 
-// ejemplo 4: creacion de frases promocionales 
+  // ejemplo 4: creacion de frases promocionales 
 
-// let adjetivos = ["Increible", "Fantastico", "Unico"]
-// let productos = ["Telefono", "Reloj", "Auriculares", "Televisor"]
-// let ofertas = ["En descuento", "Sin descuento"]
+  // let adjetivos = ["Increible", "Fantastico", "Unico"]
+  // let productos = ["Telefono", "Reloj", "Auriculares", "Televisor"]
+  // let ofertas = ["En descuento", "Sin descuento"]
 
-// for (let i = 0; i < adjetivos.length; i++) {
+  // for (let i = 0; i < adjetivos.length; i++) {
 
-//   for (let j = 0; j < productos.length; j++) {
-//     console.log(adjetivos[i] + " " + productos[j] + " Compra ya!! ");
-    
-    
-//   }
-  
-// }
-
-///////////////////////////////////////////////////////////
-// bucle while
-
-// while (condicion){
-//   //codigo a ejecutar
-// }
-
-// let contador = 0
-// while (contador < 5) {
-//   console.log(contador);
-//   contador ++ // aumenta el contador en 1 para que la condicion se vuelva falsa eventualmente 
-// }
-///ejemplo 
-// let numero = 5
-// while (numero > 0) {
-//   console.log("Quedan " + numero + " segundos...");
-//   numero-- // numero = numero -1
-// }
-// console.log("Despegue!!");
+  //   for (let j = 0; j < productos.length; j++) {
+  //     console.log(adjetivos[i] + " " + productos[j] + " Compra ya!! ");
 
 
+  //   }
 
+  // }
 
+  ///////////////////////////////////////////////////////////
+  // bucle while
 
-///funciones//////////////
-// funcion de declaracion 
-function listaDeFrutas() {
-  let frutas = ["manzana", "naranja", "uva", "limon"]
-}
-listaDeFrutas()
+  // while (condicion){
+  //   //codigo a ejecutar
+  // }
 
-
-function saludar() {
-  console.log("Hola Mundo");
-}
-saludar()
-
-//ejemplo llamada de funcion antes de la declaracion 
-suma()
-
-function suma() {
-  let resultado = 4 + 5
-  console.log(resultado);
-  
-  
-}
-
-//ejemplo con parametros
-function multiplicar(x,y) {
-  return x * y
-
-}
-let producto = multiplicar(5,10)
-console.log(producto);
-
-let producto1 = multiplicar(3,8)
-console.log(producto1);
-
-//ejemplo:
-function saludo(nombre) {
-  console.log( "hola, " + nombre + "!");
-  
-}
-saludo ("Julio") 
-saludo ("Maria") 
-saludo ("Jose") 
-
-
-/////////////////////////////funciones de expresion - funcion anonima
-
-let restar = function(a, b) {
-  return a - b
-}
-let diferencia = restar(10, 3)
-console.log(diferencia);
-
-//ejemplo:
-
-let saludar2 = function() {
-  console.log("hola desde una funcion de expresion");
-  
-}
-saludar2()
-
-//////////////// funcion flecha 
-// ejemplo:
-const duplicar = numero => numero * 2
-let resultadoDeDuplicar = duplicar(8)
-console.log(resultadoDeDuplicar);
-
-// ejemplo :
-const calcularIva = (precio) => {
-   const iva = 0.16
-   return precio * (1 + iva)
-}
-let precioTotal= calcularIva(200)
-console.log(precioTotal);
+  // let contador = 0
+  // while (contador < 5) {
+  //   console.log(contador);
+  //   contador ++ // aumenta el contador en 1 para que la condicion se vuelva falsa eventualmente 
+  // }
+  ///ejemplo 
+  // let numero = 5
+  // while (numero > 0) {
+  //   console.log("Quedan " + numero + " segundos...");
+  //   numero-- // numero = numero -1
+  // }
+  // console.log("Despegue!!");
 
 
 
-//Math.floor(Math.random())
-             // 0   1     2    3    4
-let vocales = ["a", "e", "i", "o", "u"]
-//generar un indice aleatorio
-let aleatorio = Math.floor(Math.random() * vocales.length)
 
-console.log(vocales[aleatorio]);
+
+  ///funciones//////////////
+  // funcion de declaracion 
+  // function listaDeFrutas() {
+  //   let frutas = ["manzana", "naranja", "uva", "limon"]
+  // }
+  // listaDeFrutas()
+
+
+  // function saludar() {
+  //   console.log("Hola Mundo");
+  // }
+  // saludar()
+
+  // //ejemplo llamada de funcion antes de la declaracion 
+  // suma()
+
+  // function suma() {
+  //   let resultado = 4 + 5
+  //   console.log(resultado);
+
+
+  // }
+
+  // //ejemplo con parametros
+  // function multiplicar(x,y) {
+  //   return x * y
+
+  // }
+  // let producto = multiplicar(5,10)
+  // console.log(producto);
+
+  // let producto1 = multiplicar(3,8)
+  // console.log(producto1);
+
+  // //ejemplo:
+  // function saludo(nombre) {
+  //   console.log( "hola, " + nombre + "!");
+
+  // }
+  // saludo ("Julio") 
+  // saludo ("Maria") 
+  // saludo ("Jose") 
+
+
+  // /////////////////////////////funciones de expresion - funcion anonima
+
+  // let restar = function(a, b) {
+  //   return a - b
+  // }
+  // let diferencia = restar(10, 3)
+  // console.log(diferencia);
+
+  // //ejemplo:
+
+  // let saludar2 = function() {
+  //   console.log("hola desde una funcion de expresion");
+
+  // }
+  // saludar2()
+
+  // //////////////// funcion flecha 
+  // // ejemplo:
+  // const duplicar = numero => numero * 2
+  // let resultadoDeDuplicar = duplicar(8)
+  // console.log(resultadoDeDuplicar);
+
+  // // ejemplo :
+  // const calcularIva = (precio) => {
+  //    const iva = 0.16
+  //    return precio * (1 + iva)
+  // }
+  // let precioTotal= calcularIva(200)
+  // console.log(precioTotal);
+
+
+
+  // //Math.floor(Math.random())
+  //              // 0   1     2    3    4
+  // let vocales = ["a", "e", "i", "o", "u"]
+  // //generar un indice aleatorio
+  // let aleatorio = Math.floor(Math.random() * vocales.length)
+
+  // console.log(vocales[aleatorio]);
 
 };
+
+//tipos de dato en js: string, numericos, booleanos, null , arrays, funciones, 
+
+
+//objetos literales
+
+// {propiedad: "valor" }
+
+//declaracion del objeto:
+let persona = {
+  nombre: "Ana",
+  edad: 30,
+  esEstudiante: true,
+  cursos: ["HTML", "CSS", "JS"],
+  //las funciones cuando se encuentran dentro de un objeto se llaman metodos 
+  hablar: function () {
+    return `Hola, mi nombre es ${this.nombre}, tengo ${this.edad} años y estoy estudiando ${this.cursos.length} cursos`
+  },
+  direccion: {
+    calle: "Libertador",
+    ciudad: "Caracas"
+  },
+  "fecha de nacimiento": "20-08-1995"
+}
+
+// consultar:
+//notacion de punto .
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.hablar());
+console.log(persona.direccion.ciudad);
+// console.log(persona.fecha de nacimiento ); no es correcto 
+
+
+//notacion de corchetes []
+console.log(persona.cursos[1]);
+console.log(persona["fecha de nacimiento"]);
+//ultimo curso de ana
+console.log(persona.cursos[persona.cursos.length - 1]);
+
+
+
+// 
+//templates literals
+
+// ` ${expresion}`
+let nombre = "Maria"
+let edad = 28
+
+let saludo = `Hola, mi nombre es ${nombre} y tengo ${edad} años.`
+console.log(saludo);
+
+
+
+//operadores ternarios
+
+// la sintaxis es : condicion ? expresion_si_es_verdadero : expresion_si_es_falso
+
+
+// let edades = 10
+// let puedeVotar = edades >=18 ? "si puede votar" : "no puede votar"
+// console.log(puedeVotar);
+
+///agrego mas condicion:
+
+// condicion1 ? valor_si_es_verdadera1 : (condicion2 ? valor_si_es_verdadera2 : valor_si_es_falsa2)
+
+
+let edades = 82
+
+let puedeVotar = (edades >= 80)
+              ? "muy mayor para votar" 
+              : (edades >= 18) 
+              ? "puede votar" 
+              : "no puede votar";
+console.log(puedeVotar);
+
+
+
+
+//ejemplo 2
+let a = 10
+let b = 5
+let resultado = a > b ? a * 2 : b / 2
+console.log(resultado);
